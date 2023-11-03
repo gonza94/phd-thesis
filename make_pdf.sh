499540515fbf34cef6fb58aedabee66228dfe7cb
gonza94 <gonza839@msu.edu>
2023-11-03 16:38:42 -0500

    Progress from Friday November 3rd. Fixed biber issue and cache issue. Everything compiling now.

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,5 +1,12 @@
-pdflatex phd-thesis
-biber phd-thesis
-pdflatex phd-thesis
-pdflatex phd-thesis
+if [ "$1" = "--clearcache" -o "$1" = "-cc" ]; then
+    echo "Cleaning biber cache"
+    rm -rf $(biber --cache)
+else
+    break
+fi
+
+pdflatex -synctex=1 -interaction=nonstopmode "phd-thesis".tex
+biber -D "phd-thesis"
+pdflatex -synctex=1 -interaction=nonstopmode "phd-thesis".tex
+pdflatex -synctex=1 -interaction=nonstopmode "phd-thesis".tex
 open phd-thesis.pdf
